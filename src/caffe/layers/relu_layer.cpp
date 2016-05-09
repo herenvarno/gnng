4044a11b26b2219d3c4821f2e7ec4ebfdf545039
herenvarno <herenvarno@live.com>
2016-05-09 16:48:53 +0200

    init commit

---
+++ src/caffe/layers/relu_layer.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <vector>
+
+#include "caffe/layers/relu_layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void ReLULayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mutable_cpu_data();
+  const int count = bottom[0]->count();
+  Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
+  for (int i = 0; i < count; ++i) {
+    top_data[i] = std::max(bottom_data[i], Dtype(0))
+        + negative_slope * std::min(bottom_data[i], Dtype(0));
+  }
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(ReLULayer);
+#endif
+
+INSTANTIATE_CLASS(ReLULayer);
+REGISTER_LAYER_CLASS(ReLU); 
+
+}  // namespace caffe
